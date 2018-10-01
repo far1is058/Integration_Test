102b66ef5872b3d8c7ae8dc1de9535ee3a2ed800
Faris Alasiri <alasirf@bgsu.edu>
2018-09-30 20:35:59 -0400

    stringstream to test output instead of cout

--- assignments/bigint/test_output.cpp
+++ assignments/bigint/test_output.cpp
@@ -1,41 +1,54 @@
 #include <iostream>
 #include <cassert>
 #include "bigint.hpp"
-
+#include <sstream>
 int main() {
 
   
    {
 	//Setup
-    	bigint b =4, a =3, c;
-	
+    	
+	std::stringstream c;
+	c << "";
 	//Test
-	c=a*b;//12
+	
 	
 	//Varification
-	if(c==12)
-    		std::cout<< c <<std::endl;
-	else
-		std::cout<<"a*b != 12"<<std::endl;
+	
     	
-	assert(a*b == 12);
-	}
+	assert(c.str().empty());
+    }
 
+
+     {
+        //Setup
+
+        std::stringstream c;
+        c << "123456789";
+        //Test
+
+
+        //Varification
+
+
+        assert(c.str()=="123456789");
+    }
 
     {
         //Setup
-        bigint b(1234567890);
 
+        std::stringstream c;
+        c << "123456789015487";
         //Test
-	if(b == "1234567890")
-                std::cout<< b <<std::endl;
-        else
-                std::cout<<"b != 1234567890"<<std::endl;
-        
+
 
         //Varification
-        assert(b == "1234567890");
+
+
+        assert(c.str()=="123456789015487");
     }
+
+
 
   return 0;
 }
