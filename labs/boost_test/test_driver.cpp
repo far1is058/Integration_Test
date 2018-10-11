649b341da54c06db03cf687d16f61e667baf20a9
Faris Alasiri <alasirf@bgsu.edu>
2018-10-11 12:23:36 -0400

    forgotten and added for regrade

---
+++ labs/boost_test/test_driver.cpp
@@ -0,0 +1,14 @@
+#define BOOST_TEST_MODULE String
+#include <boost/test/included/unit_test.hpp>
+#include <boost/test/data/monomorphic.hpp>
+#include <boost/test/data/test_case.hpp>
+
+#include <iostream>
+
+#include "string.hpp"
+
+namespace bu = boost::unit_test;
+
+#include "test_default_ctor.hpp"
+#include "test_cstring_ctor.hpp"
+#include "test_equality.hpp"
